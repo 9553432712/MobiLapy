ec087c72fc03943d5b5492988d526aff516c54a8
Mahesh Punugupati <mahesh.punugupati@trianz.com>
2019-05-03 11:11:52 +0530

    first commit

---
+++ src/main/java/com/MobiLapy/MobiLapy/MobiLapyApplication.java
@@ -0,0 +1,13 @@
+package com.MobiLapy.MobiLapy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobiLapyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobiLapyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MobiLapy/MobiLapy/MobiLapyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.MobiLapy.MobiLapy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MobiLapyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
